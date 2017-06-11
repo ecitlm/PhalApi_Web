2cbb11f7feb0b8f2d9b58a489807af0368031030
ecitlm <ecitlm@163.com>
2017-06-11 14:38:41 +0800

    data init push

---
+++ PhalApi/PhalApi/Filter/SimpleMD5.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * PhalApi_Filter_SimpleMD5 简单的MD5拦截器
+ *
+ * - 签名的方案如下：
+ *
+ * + 1、排除签名参数（默认是sign）
+ * + 2、将剩下的全部参数，按参数名字进行字典排序
+ * + 3、将排序好的参数，全部用字符串拼接起来
+ * + 4、进行md5运算
+ *
+ * 注意：无任何参数时，不作验签
+ *
+ * @package     PhalApi\Filter
+ * @license     http://www.phalapi.net/license GPL 协议
+ * @link        http://www.phalapi.net/
+ * @author      dogstar <chanzonghuang@gmail.com> 2015-10-23
+ */
+
+class PhalApi_Filter_SimpleMD5 implements PhalApi_Filter {
+
+    protected $signName;
+
+    public function __construct($signName = 'sign') {
+        $this->signName = $signName;
+    }
+
+    public function check() {
+
+        $service = DI()->request->get('service');
+        $app=DI()->config->get('app');
+        $apiFilterRules = $app['apiFilterRules'];
+        if (!in_array($service,$apiFilterRules)) {
+            $allParams = DI()->request->getAll();
+
+            $sign = isset($allParams[$this->signName]) ? $allParams[$this->signName] : '';
+            unset($allParams[$this->signName]);
+            unset($allParams['service']);
+
+            //加上appkey
+            $sys = DI()->config->get('sys');
+            $allParams['appkey'] = $sys['appkey'];
+            $expectSign = $this->encryptAppKey($allParams);
+
+            if ($expectSign != $sign) {
+                DI()->logger->debug('Wrong Sign', array('needSign' => $expectSign));
+                throw new PhalApi_Exception_BadRequest(T('wrong sign'), 6);
+            }
+        }
+    }
+
+    protected function encryptAppKey($params) {
+        ksort($params);
+
+        $paramsStrExceptSign = '';
+        foreach ($params as $val) {
+            $paramsStrExceptSign .= $val;
+        }
+
+        return MD5($paramsStrExceptSign);
+
+    }
+}
---
+++ Web/Api/Index.php
@@ -0,0 +1,70 @@
+<?php
+/**
+* Index 模块服务接口
+*/
+
+class Api_Index extends PhalApi_Api {
+
+    /**
+     * 定义路由规则
+     * @return array
+     */
+    public function getRules() {
+        return array(
+            'query' => array(
+                //'page' => array('name' => 'page', 'type' => 'int', 'min' => 1, 'default' => '1', 'require' => false, 'desc' => '分页'),
+              
+            ),
+        );
+    }
+
+
+    /**
+     * 获取163数据
+     * @method POST请求
+     * @desc 获取163数据
+     * @url http://192.168.1.2:8097/Public/?service=Index.index
+     */
+    public function index(){
+
+        $res=DI()->base->HttpGet("http://c.3g.163.com/nc/article/list/T1348648517839/0-20.html");
+        return json_decode($res);
+    }
+
+
+    /**
+     *
+     * @return string title 商品id
+     * @return int daily_id   详情id
+     * @return string des  描述
+     * @return string date  时间日期
+     */
+    public function query(){
+       // $page=$this->page;
+        $url = "http://caibaojian.com/c/news";
+        $res=DI()->base->HttpGet($url);
+        \phpQuery::newDocumentHTML($res);
+
+        $arr = array();
+        $list = pq('#content article');
+        foreach ($list as $li) {
+            $title = pq($li)->find('.entry-title span')->text();
+            $desc = pq($li)->find('.entry-content p')->text();
+            $url = pq($li)->find('.read-more')->attr('href');
+            $date = pq($li)->find('.entry-date')->text();
+            $id = intval(preg_replace('/\D/s', '', $url));
+
+            $tmp = array(
+                'title' => $title,
+                'date' => $date,
+                'desc' => $desc,
+                'daily_id' => $id
+            );
+            array_push($arr, $tmp);
+        }
+
+        return $arr;
+
+    }
+
+}
